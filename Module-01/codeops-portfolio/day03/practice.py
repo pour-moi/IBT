7214680d45268277641ff85cbb07bb66eb8cda7b
questc0der <pour64moi@gmail.com>
2026-07-11 23:56:01 +0300

    day03 - unique cities assignment

---
+++ Module-01/codeops-portfolio/day03/practice.py
@@ -0,0 +1,4 @@
+#Unique cities
+cities = ["Addis Ababa", "Adama","Bahir Dar","Mekelle","Hawassa","Addis Ababa","Jimma","Bahir Dar","Dire Dawa","Hawassa"]
+unique = {city for city in cities}
+print(unique)

972d79e5f724e291304f837d9541e8b709a8bd84
questc0der <pour64moi@gmail.com>
2026-07-12 02:47:06 +0300

    done tax comperhension

--- Module-01/codeops-portfolio/day03/practice.py
+++ Module-01/codeops-portfolio/day03/practice.py
@@ -1,11 +1,16 @@
 #Unique cities
-cities = ["Addis Ababa", "Adama","Bahir Dar","Mekelle","Hawassa","Addis Ababa","Jimma","Bahir Dar","Dire Dawa","Hawassa"]
-unique = {city for city in cities}
-count = {city: cities.count(city) for city in unique}
-print(count)
-print(unique)
+# cities = ["Addis Ababa", "Adama","Bahir Dar","Mekelle","Hawassa","Addis Ababa","Jimma","Bahir Dar","Dire Dawa","Hawassa"]
+# unique = {city for city in cities}
+# count = {city: cities.count(city) for city in unique}
+# print(count)
+# print(unique)
 
 #Price Report
-groceries = {"banana": 25, "Avocado": 30, "Tomato": 45, "Orange": 30, "apple": 150}
-for grocery in groceries:
-    print(f"{grocery}: {groceries[grocery]}")
+# groceries = {"banana": 25, "Avocado": 30, "Tomato": 45, "Orange": 30, "apple": 150}
+# for grocery in groceries:
+#     print(f"{grocery}: {groceries[grocery]}")
+
+#Tax comperhension
+prices = [100, 250, 400, 80]
+tax = [p * 1.15 for p in prices]
+print(tax)
